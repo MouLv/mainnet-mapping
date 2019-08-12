b066f4f87d749dc27a69f3a09c8df6601bd8776b
fanliangqin <fanliangqin@ultrain.io>
2019-08-12 10:53:15 +0800

    refactor recheck_holders.js

--- tools/README.md
+++ tools/README.md
@@ -1,20 +1,27 @@
 # 主网映射前准备工作的工具
 
+主网映射一共分为4步:
+1. 获取所有持币人列表.
+2. 将持币账号分类为合约账号/持币账号.
+3. 通过ustoke合约, 读取持币账号的register过的ultrain账号, 以及balance.
+4. 在ultrian主链上, 将balance转入register的ultrain账号.  
+
 ## Dependencies
 1. web2@0.20.1      # 如果已经安装其它版本的web3, 请先`npm -g uninstall web3`删除, 再`npm install -g web3@0.20.1`  
 2. request-promise    # npm install -g request-promise
 
 ### fetch_holders.js
 这个文件来用获取所有的持币账号.  
-`node fetch_holders.js`  # 生成文件 fetch-holders-xx.json
+`node fetch_holders.js`  # 生成文件 fetch-holders.json
 
 ### classify_holders.js
-这个文件来用分类持币账号, 主要分成两大类: 正常持币人账号和合约账号
-`node classify_holders.js ./fetch-holders-xx.json`   # 生成文件 classify-holders-xx.json
+这个文件来用分类持币账号, 主要分成两大类: 正常持币人账号和合约账号.  
+`node classify_holders.js ./fetch-holders.json ./classify-holders.json`   # 生成文件 classify-holders.json
 
 ### recheck_holders.js
-这个文件从ustoke合约中查询以下信息: 已经映射的账号, 未映射的账号, 己映射但余额为0的账号  
-`node recheck_holders.js ./classify-holders-xx.json`   # 生成文件 rechecked-holders-xx.json
+这个文件从ustoke合约中查询以下信息: 已经映射的账号, 未映射的账号, 己映射但余额为0的账号.  
+`node recheck_holders.js ./classify-holders.json ./rechecked-holders.json`   # 生成文件 rechecked-holders.json
 
-拿到rechecked-holders-xx.json文件后, 转账前的信息收集就已经完成了.  
+### 主网打币(待完成)
+拿到rechecked-holders.json文件后, 转账前的信息收集就已经完成了.  
 可以进行后续的主网打币工作.
--- tools/recheck_holders.js
+++ tools/recheck_holders.js
@@ -3,24 +3,66 @@
 const fs = require('fs');
 const ugas = require('./ustoke');
 
-const holders_list = process.argv[2];
-if (!fs.existsSync(holders_list)) {
-    console.log(`${holders_list} is not exist for holder list.`);
+const holders_list_file = process.argv[2]; // classified holders result.
+if (!fs.existsSync(holders_list_file)) {
+    console.log(`${holders_list_file} is not exist for holder list.`);
     process.exit(1);
 }
 
-// 只处理非合约账号
-let result = {
+const handled_holders_file = process.argv[3];
+let handled_holders = {
     validated: [], // 已经映射, 且blance > 0
     mapped:[],    // 已经映射, 且balance = 0
     unmapped:[]   // 未映射
 };
 
-const list = require(holders_list);
+if (fs.existsSync(handled_holders_file)) {
+    handled_holders = require(handled_holders_file)
+}
+
+const RECHECKED_FILE = 'rechecked-holders';
+// 只处理非合约账号
+let temp_handled = {
+    validated: [], // 已经映射, 且blance > 0
+    mapped:[],    // 已经映射, 且balance = 0
+    unmapped:[]   // 未映射
+};
+
+const isHandled = (addr) => {
+    for (let v of handled_holders.validated) {
+        if (v.address === addr) return true;
+    }
+
+    for (let m of handled_holders.mapped) {
+        if (m.address === addr) return true;
+    }
+
+    for (let u of handled_holders.unmapped) {
+        if (u.address === addr) return true;
+    }
+
+    return false;
+};
+
+const saveTempHandled = () => {
+    handled_holders.validated = handled_holders.validated.concat(temp_handled.validated);
+    handled_holders.mapped = handled_holders.mapped.concat(temp_handled.mapped);
+    handled_holders.unmapped = handled_holders.unmapped.concat(temp_handled.unmapped);
+
+    ugas.saveFile(RECHECKED_FILE, handled_holders);
+
+    temp_handled.validated = [];
+    temp_handled.mapped = [];
+    temp_handled.unmapped = [];
+}
+
+const list = require(holders_list_file);
 const holders = list.holders;
 let i = 1;
 for (let holder of holders) {
     console.log(`Handling  ${i++}/${holders.length}`);
+
+    if (isHandled(holder)) continue;
 
     let r = {
         address: holder,
@@ -34,15 +76,17 @@ for (let holder of holders) {
     if (typeof x === 'string' && x.length != 0) {
         r.utraddress = x;
         r.balance = y;
-        if (y === 0) {
-            result.mapped.push(r);
+        if (y === 0) { // 余额等于0, 不处理
+            temp_handled.mapped.push(r);
         } else {
-            result.validated.push(r);
+            temp_handled.validated.push(r);
         }
     } else {
         r.balance = y;
-        result.unmapped.push(r);
+        temp_handled.unmapped.push(r);
     }
+
+    if (i % 50 == 0) saveTempHandled();
 }
 
-ugas.saveFile('rechecked-holders', result);
+ugas.saveFile(RECHECKED_FILE, handled_holders);
